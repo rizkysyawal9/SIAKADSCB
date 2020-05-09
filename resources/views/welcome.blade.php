9f1a0ead0e77f0c57b9721eddac0e460d79c980f
rizkysyawal9 <rizky_syawal@apps.ipb.ac.id>
2020-05-09 19:06:37 +0700

    added vue so website go brrr

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,10 @@
     </head>
     <body>
 
-        <div id="hello">Home</div>
+        <div id="app">Home</div>
         <div id="example"></div>
     </body>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script src="{{ asset('js/app') }}"></script>
 </html>
